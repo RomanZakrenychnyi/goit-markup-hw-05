55791fecfdb75405fe05ee3df5b6c688fbffcaec
RomanZakrenychnyi <116659319+RomanZakrenychnyi@users.noreply.github.com>
2022-11-26 15:10:11 +0200

    work on modal window

--- index.js
+++ index.js
@@ -9,6 +9,6 @@
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden"); document.body.classList.toggle("modal-open");
+    refs.modal.classList.toggle("is-hidden");document.body.classList.toggle("modal-open");
   }
 })();
